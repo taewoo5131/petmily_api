6f1193a4df6ad1d0c4bb8a1eecb1996c664e6161
leetaewoo <taewoo5131@gmail.com>
2022-12-04 11:59:49 +0900

    20221204 - 캘린더 수정 , 삭제 구현완료

--- src/main/java/com/petmily/api/repository/CalendarRepository.java
+++ src/main/java/com/petmily/api/repository/CalendarRepository.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import java.util.Map;
 
 @Slf4j
 @Repository
@@ -21,21 +19,5 @@ public class CalendarRepository {
         em.persist(calendar);
         em.flush();
         return em.find(Calendar.class , calendar.getId());
-    }
-
-    public Long update(Map<String, Object> paramMap) {
-        Calendar findCalendar = em.find(Calendar.class, Long.parseLong(paramMap.get("calendarIdx").toString()));
-        findCalendar.setTargetName(paramMap.get("targetName").toString());
-        findCalendar.setTargetDate(paramMap.get("targetDate").toString());
-        return findCalendar.getId();
-    }
-
-    public Long delete(Map<String, Object> paramMap) {
-        Calendar findCalendar = em.createQuery("select c from Calendar c join fetch c.family f where f.idx =: familyIdx and c.id =: calendarIdx", Calendar.class)
-                .setParameter("familyIdx", Long.parseLong(paramMap.get("familyIdx").toString()))
-                .setParameter("calendarIdx", Long.parseLong(paramMap.get("calendarIdx").toString()))
-                .getSingleResult();
-        em.remove(findCalendar);
-        return Long.parseLong(paramMap.get("calendarIdx").toString());
     }
 }
