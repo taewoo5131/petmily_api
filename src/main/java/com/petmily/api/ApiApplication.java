f44c28a3c0051e0d5a72e12b5b4a44f55dc89fbd
leetaewoo <taewoo5131@gmail.com>
2022-12-12 16:07:02 +0900

    20221212 - front localhost cors 방지

--- src/main/java/com/petmily/api/ApiApplication.java
+++ src/main/java/com/petmily/api/ApiApplication.java
@@ -3,6 +3,9 @@ package com.petmily.api;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -12,4 +15,13 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:5500").allowedOrigins("http://127.0.0.1:5500");
+			}
+		};
+	}
 }
